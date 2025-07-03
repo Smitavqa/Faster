f0eadec9722826ef6f59d345a3f18b0b626bd0b4
Smitavqa <smitav.qa@gmail.com>
2025-07-03 12:42:41 -0800

    Adding entire project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Faster
+This is frame work for Faster project
+<ul>
+<li>Key Driven framework </li>
+<li> Page Object Model </li>
+</ul>
---
+++ src/test/java/faster/smart/smartSmita.java
@@ -0,0 +1,10 @@
+package faster.smart;
+
+public class smartSmita {
+
+	public static void main(String[] args) {
+		System.out.println("Smart smita here");
+
+	}
+
+}
