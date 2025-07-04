2dec3936c4c48f3ecf93a598a18d0a1507d5d60e
Smita Vharavadekar <smita.vharavadekar@pdc.com>
2025-07-04 15:52:06 +0530

    Two main1 nad main2 added

--- src/test/java/faster/smart/A.java
+++ src/test/java/faster/smart/A.java
@@ -4,6 +4,12 @@ public class A {
 	
 	public static void main1() {
 		System.out.println("I am from A class main1");
+		System.out.println("My Method name is main1");
+	}
+	
+	public static void main2() {
+		System.out.println("I am from A class main2");
+		System.out.println("My Method name is main2");
 	}
 	
 
