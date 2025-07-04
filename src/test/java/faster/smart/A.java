e4b023dce1157bdd710f972aac66e11e14bad62a
Smita Vharavadekar <smita.vharavadekar@pdc.com>
2025-07-04 14:36:11 +0530

    A file added from smita office laptop for test

---
+++ src/test/java/faster/smart/A.java
@@ -0,0 +1,16 @@
+package faster.smart;
+
+public class A {
+	
+	public static void main1() {
+		System.out.println("I am from A class ");
+	}
+	
+
+	public static void main(String[] args) {
+
+
+
+	}
+
+}
